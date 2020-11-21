9f7accc17b7be68ad6ee7922ffe9f8a1ef50766e
Arco Mul <me@arcomul.nl>
2020-11-21 16:52:23 +0100

    Load local files into dev Docker container + remove YIPP from config options

--- modules/server/lib/config.js
+++ modules/server/lib/config.js
@@ -3,18 +3,18 @@ const configuratoror = require('configuratoror')
 const options = {
   folder: './etc'
 }
-const yippConfig = process.env.YIPP_CONFIG
+const selectedConfig = process.env.CONFIG
 const env = process.env.NODE_ENV
 
-if (!yippConfig) {
+if (!selectedConfig) {
   throw new Error(
     'No YIPP_CONFIG given, define YIPP_CONFIG=<environment> before running server'
   )
 }
 
-console.log(`Running with config: ${yippConfig}`)
+console.log(`Running with config: ${selectedConfig}`)
 
-const config = configuratoror(options)(yippConfig)
+const config = configuratoror(options)(selectedConfig)
 config.env = env
 
 module.exports = config
