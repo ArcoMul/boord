51505629ec06a4d41f1d892bb7f697aad30ed5a7
Arco Mul <me@arcomul.nl>
2021-02-09 17:00:46 +0100

    Add bash file to easily push image to docker

---
+++ publish.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Start building Docker image"
+docker build -f Dockerfile.prod -t lumocra/boord:latest .
+echo "Finished building Docker image"
+echo "Start pushing Docker image"
+docker push lumocra/boord:latest
+echo "Finished pushing Docker image"
