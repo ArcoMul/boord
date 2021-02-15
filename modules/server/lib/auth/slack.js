66830b4bd27b0bad241d0127bafc7754bc736a88
Arco Mul <me@arcomul.nl>
2021-02-15 21:28:49 +0100

    Update user avatar when there is a new slack profile image

--- modules/server/lib/auth/slack.js
+++ modules/server/lib/auth/slack.js
@@ -1,13 +1,12 @@
 const SlackStrategy = require('@aoberoi/passport-slack')
 const User = require('../../schemas/User')
-const config = require('../config')
 
-const slackStrategy = function () {
+const slackStrategy = function() {
   return new SlackStrategy.default.Strategy(
     {
       clientID: process.env.SLACK_CLIENT_ID,
       clientSecret: process.env.SLACK_CLIENT_SECRET,
-      scope: ['identity.basic', 'identity.email', 'identity.avatar'],
+      scope: ['identity.basic', 'identity.email', 'identity.avatar']
     },
     async (
       accessToken,
@@ -25,7 +24,7 @@ const slackStrategy = function () {
           email: userProfile.email,
           avatar: userProfile.image_192,
           slackId: userProfile.id,
-          permissions: ['user', 'admin'],
+          permissions: ['user', 'admin']
         })
       } else if (!user.email) {
         user = await User.findByIdAndUpdate(
@@ -33,14 +32,21 @@ const slackStrategy = function () {
           {
             $set: {
               email: userProfile.email,
-              avatar: userProfile.image_192,
-            },
+              avatar: userProfile.image_192
+            }
           },
           {
-            new: true,
+            new: true
           }
         )
       }
+
+      // If there is a new profile image, save it
+      if (user.avatar !== userProfile.image_192) {
+        user.avatar = userProfile.image_192
+        await user.save()
+      }
+
       return done(null, user)
     }
   )
